2012654edcbe6d1ab8cb7fa79132ff63e4ee8592
Cory LePage <cory@razorcx.com>
2017-12-27 22:17:14 -0800

    Initial commit

---
+++ ShowParts/ShowPartsForm.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ShowParts
+{
+	partial class ShowParts
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ShowParts));
+			this.buttonSelectPartsInModel = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.listBoxMainPartNames = new System.Windows.Forms.ListBox();
+			this.comboBox1ShowSettings = new System.Windows.Forms.ComboBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.checkBoxShowSecondaries = new System.Windows.Forms.CheckBox();
+			this.checkBoxBolts = new System.Windows.Forms.CheckBox();
+			this.checkBoxCutsFittings = new System.Windows.Forms.CheckBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// buttonSelectPartsInModel
+			// 
+			this.buttonSelectPartsInModel.Location = new System.Drawing.Point(233, 290);
+			this.buttonSelectPartsInModel.Name = "buttonSelectPartsInModel";
+			this.buttonSelectPartsInModel.Size = new System.Drawing.Size(151, 33);
+			this.buttonSelectPartsInModel.TabIndex = 17;
+			this.buttonSelectPartsInModel.Text = "Select Parts";
+			this.buttonSelectPartsInModel.UseVisualStyleBackColor = true;
+			this.buttonSelectPartsInModel.Click += new System.EventHandler(this.buttonSelectPartsInModel_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 74);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(82, 17);
+			this.label1.TabIndex = 16;
+			this.label1.Text = "Part Names";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::ShowParts.Properties.Resources.Logo;
+			this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(160, 41);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 14;
+			this.pictureBox1.TabStop = false;
+			// 
+			// listBoxMainPartNames
+			// 
+			this.listBoxMainPartNames.FormattingEnabled = true;
+			this.listBoxMainPartNames.ItemHeight = 16;
+			this.listBoxMainPartNames.Location = new System.Drawing.Point(12, 95);
+			this.listBoxMainPartNames.Name = "listBoxMainPartNames";
+			this.listBoxMainPartNames.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+			this.listBoxMainPartNames.Size = new System.Drawing.Size(214, 228);
+			this.listBoxMainPartNames.TabIndex = 18;
+			// 
+			// comboBox1ShowSettings
+			// 
+			this.comboBox1ShowSettings.FormattingEnabled = true;
+			this.comboBox1ShowSettings.Location = new System.Drawing.Point(233, 95);
+			this.comboBox1ShowSettings.Name = "comboBox1ShowSettings";
+			this.comboBox1ShowSettings.Size = new System.Drawing.Size(150, 24);
+			this.comboBox1ShowSettings.TabIndex = 19;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(230, 74);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(90, 17);
+			this.label2.TabIndex = 20;
+			this.label2.Text = "Show Setting";
+			// 
+			// checkBoxShowSecondaries
+			// 
+			this.checkBoxShowSecondaries.AutoSize = true;
+			this.checkBoxShowSecondaries.Location = new System.Drawing.Point(233, 136);
+			this.checkBoxShowSecondaries.Name = "checkBoxShowSecondaries";
+			this.checkBoxShowSecondaries.Size = new System.Drawing.Size(147, 21);
+			this.checkBoxShowSecondaries.TabIndex = 21;
+			this.checkBoxShowSecondaries.Text = "Show Secondaries";
+			this.checkBoxShowSecondaries.UseVisualStyleBackColor = true;
+			// 
+			// checkBoxBolts
+			// 
+			this.checkBoxBolts.AutoSize = true;
+			this.checkBoxBolts.Location = new System.Drawing.Point(233, 163);
+			this.checkBoxBolts.Name = "checkBoxBolts";
+			this.checkBoxBolts.Size = new System.Drawing.Size(99, 21);
+			this.checkBoxBolts.TabIndex = 22;
+			this.checkBoxBolts.Text = "Show Bolts";
+			this.checkBoxBolts.UseVisualStyleBackColor = true;
+			// 
+			// checkBoxCutsFittings
+			// 
+			this.checkBoxCutsFittings.AutoSize = true;
+			this.checkBoxCutsFittings.Location = new System.Drawing.Point(233, 190);
+			this.checkBoxCutsFittings.Name = "checkBoxCutsFittings";
+			this.checkBoxCutsFittings.Size = new System.Drawing.Size(145, 21);
+			this.checkBoxCutsFittings.TabIndex = 23;
+			this.checkBoxCutsFittings.Text = "Show Cuts/Fittings";
+			this.checkBoxCutsFittings.UseVisualStyleBackColor = true;
+			// 
+			// ShowParts
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(395, 341);
+			this.Controls.Add(this.checkBoxCutsFittings);
+			this.Controls.Add(this.checkBoxBolts);
+			this.Controls.Add(this.checkBoxShowSecondaries);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.comboBox1ShowSettings);
+			this.Controls.Add(this.listBoxMainPartNames);
+			this.Controls.Add(this.buttonSelectPartsInModel);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.pictureBox1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "ShowParts";
+			this.Text = "Show Parts";
+			this.TopMost = true;
+			this.Load += new System.EventHandler(this.ShowParts_Load);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button buttonSelectPartsInModel;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.ListBox listBoxMainPartNames;
+		private System.Windows.Forms.ComboBox comboBox1ShowSettings;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.CheckBox checkBoxShowSecondaries;
+		private System.Windows.Forms.CheckBox checkBoxBolts;
+		private System.Windows.Forms.CheckBox checkBoxCutsFittings;
+	}
+}
+
---
+++ ShowParts/ShowPartsForm.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Tekla.Structures.Model;
+using Tekla.Structures.Model.Operations;
+using Tekla.Structures.Model.UI;
+
+namespace ShowParts
+{
+	public partial class ShowParts : Form
+	{
+		private readonly Model _model = new Model();
+
+		public ShowParts()
+		{
+			InitializeComponent();
+		}
+
+		private void ShowParts_Load(object sender, EventArgs e)
+		{
+			var mainParts = GetMainParts();
+
+			var names = mainParts.Select(p => p.Name).Distinct().ToList();
+
+			listBoxMainPartNames.DataSource = names;
+
+			var showSettings = new List<Operation.UnselectedModeEnum>
+			{
+				Operation.UnselectedModeEnum.Hidden,
+				Operation.UnselectedModeEnum.AsSticks,
+				Operation.UnselectedModeEnum.Transparent
+			};
+
+			comboBox1ShowSettings.DataSource = showSettings;
+		}
+
+		private List<Part> GetMainParts()
+		{
+			ModelObjectEnumerator.AutoFetch = true;
+
+			var beams = _model.GetModelObjectSelector()
+				.GetAllObjectsWithType(ModelObject.ModelObjectEnum.BEAM)
+				.ToAList<Beam>();
+
+			var bentPlates = _model.GetModelObjectSelector()
+				.GetAllObjectsWithType(ModelObject.ModelObjectEnum.BENT_PLATE)
+				.ToAList<BentPlate>();
+
+			var contourPlates = _model.GetModelObjectSelector()
+				.GetAllObjectsWithType(ModelObject.ModelObjectEnum.CONTOURPLATE)
+				.ToAList<ContourPlate>();
+
+			var polybeams = _model.GetModelObjectSelector()
+				.GetAllObjectsWithType(ModelObject.ModelObjectEnum.POLYBEAM)
+				.ToAList<PolyBeam>();
+
+			var parts = new List<Part>();
+
+			parts.AddRange(beams);
+			parts.AddRange(bentPlates);
+			parts.AddRange(contourPlates);
+			parts.AddRange(polybeams);
+
+			var mainParts = parts.AsParallel().Where(p =>
+			{
+				int isMainPart = 0;
+				p.GetReportProperty("MAIN_PART", ref isMainPart);
+				return isMainPart > 0;
+			}).ToList();
+
+			return mainParts;
+		}
+
+		private void buttonSelectPartsInModel_Click(object sender, EventArgs e)
+		{
+			var selectedItems = listBoxMainPartNames.SelectedItems;
+
+			var names = new List<string>();
+			foreach (var item in selectedItems)
+			{
+				names.Add(item.ToString());
+			}
+
+			var modelObjects = GetMainParts().Where(p => names.Contains(p.Name)).OfType<ModelObject>().ToList();
+
+			if (checkBoxShowSecondaries.Checked)
+			{
+				var secondaries = modelObjects
+					.Where(p => p is Part)
+					.SelectMany(p => ((Part)p).GetAssembly().GetSecondaries().OfType<ModelObject>())
+					.ToList();
+
+				modelObjects.AddRange(secondaries);
+			}
+
+			if (checkBoxBolts.Checked)
+			{
+				var bolts = modelObjects
+					.Where(p => p is Part)
+					.SelectMany(p => ((Part)p).GetBolts().ToAList<ModelObject>())
+					.ToList();
+
+				modelObjects.AddRange(bolts);
+			}
+
+			if (checkBoxCutsFittings.Checked)
+			{
+				var booleans = modelObjects
+					.Where(p => p is Part)
+					.SelectMany(p => ((Part)p).GetBooleans().ToAList<ModelObject>())
+					.ToList();
+
+				modelObjects.AddRange(booleans);
+			}
+
+			var objects = new ArrayList(modelObjects);
+			new Tekla.Structures.Model.UI.ModelObjectSelector().Select(objects);
+
+			var views = ViewHandler.GetVisibleViews().ToAList<Tekla.Structures.Model.UI.View>();
+			views.ForEach(v => ViewHandler.RedrawView(v));
+
+			Operation.ShowOnlySelected((Operation.UnselectedModeEnum)comboBox1ShowSettings.SelectedItem);
+		}
+	}
+}
